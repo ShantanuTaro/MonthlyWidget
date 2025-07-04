cce88561c0055f881602430c6e0cef826068ecff
Shantanu Taro <43516930+ShantanuTaro@users.noreply.github.com>
2025-07-05 01:08:27 +0530

    Initial Commit

---
+++ Monthly/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Monthly
+//
+//  Created by Shantanu Taro on 05/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MonthlyTests/MonthlyTests.swift
@@ -0,0 +1,16 @@
+//
+//  MonthlyTests.swift
+//  MonthlyTests
+//
+//  Created by Shantanu Taro on 05/07/25.
+//
+
+import Testing
+
+struct MonthlyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
